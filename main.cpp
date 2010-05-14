e9cc2146dce51cc13bbccca5548b67c06ae52701
Alejandro R. Mosteo <alejandro@mosteo.com>
2010-05-14 11:49:54 +0200

    nil

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <stdio>
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+    printf("Looking for brick...\n");
+    
     return 0;
 }
